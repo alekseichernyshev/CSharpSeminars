8b29a63870dca1ddbe1a65e3f7c9ef05ad867fbf
alekseichernyshev <adanayrel@mail.ru>
2022-12-01 20:43:45 +0900

    Возведение в квадрат

---
+++ Ex001/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Start");
+
+int number = 12; //  определили целое число
+int result = number * number; // вычисление квадрата числа
+Console.WriteLine (result); // вывод результата
+
+
+
+Console.WriteLine("End");
